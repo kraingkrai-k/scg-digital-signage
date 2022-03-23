38c787d3cf123fd61682b73c321192fd6f6536a2
KK <kraingkrai.kea@cpmatch.com>
2022-03-23 12:55:40 +0700

    update constant

---
+++ src/assets/icon/Icon.tsx
@@ -0,0 +1,9 @@
+import SvgIcon from "@mui/material/SvgIcon";
+
+export const IconBack = (props: any) => (
+    <SvgIcon {...props} >
+        <svg width="100%" height="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.84 6.92V8.92H3.84001L9.34001 14.42L7.92001 15.84L7.47681e-06 7.92L7.92001 -7.62939e-08L9.34001 1.42L3.84001 6.92H15.84Z" fill="#263238" />
+        </svg>
+    </SvgIcon>
+)
--- src/core/utils/constant.ts
+++ src/core/utils/constant.ts
@@ -1,20 +1,15 @@
 export const COLORS = {
-    primary: '#8BAEF9',
-    mainColor: '#263238',
-    lightBlue: '#F6F9FC',
-
-    white: '#ffffff',
-    black: '#263238',
-    disable: '#C4C4C4',
-    yellow: '#F0CB66',
-    pinkInActive: '#ECA9AB',
-    pink: '#F2878A',
-    blueInActive: '#8BAEF9',
-    blue: '#6797FF',
-    green: '#22C59F',
-    red: '#FE5E5A',
-    orange: '#F6921E'
+    red: '#EC1C24',
+    red1: '#D81A23',
+    white: '#FFFFFF',
+    greyText: '#231F20',
+    holder: '#938F8F',
+    balck: '#000000',
+    black1: '#121212',
+    gold: '#D5AF26'
 }
+
+// background: linear-gradient(180deg, #CFCFCF 0%, #E6E6E6 100%);
 
 
 
