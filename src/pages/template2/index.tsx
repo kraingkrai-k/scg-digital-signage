8a3b04645b8a24d7ac7ee5a1a9df711730aa1e35
KK <kraingkrai.kea@cpmatch.com>
2022-03-24 23:35:17 +0700

    workaround demo p. [1 2] update video

--- src/pages/template2/index.tsx
+++ src/pages/template2/index.tsx
@@ -23,14 +23,14 @@ const Template2: React.FC = (): React.ReactElement => {
     let timeout: any
 
     useEffect(() => {
-        // clearTimeout(timeout)
-        // timeout = setTimeout(() => {
-        //     push('/')
-        // }, TIME_OUT)
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            push('/')
+        }, TIME_OUT)
 
-        // return () => {
-        //     clearTimeout(timeout)
-        // }
+        return () => {
+            clearTimeout(timeout)
+        }
     }, [isStillActive])
 
     const handlerStillActive = () => {
@@ -46,7 +46,7 @@ const Template2: React.FC = (): React.ReactElement => {
 
     return (
         <Box
-            sx={{width: '100%', height: '100vh'}}
+            sx={{width: '100%', height: '100vh', position: 'relative'}}
             onClick={handlerStillActive}
         >
             <SvgIcon
@@ -58,6 +58,38 @@ const Template2: React.FC = (): React.ReactElement => {
                 }}
                 viewBox={`0 0 ${width} ${adjustedHeight}`}
             />
+
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: {
+                        lg: '30.4%',
+                        xl: '30.5%',
+                        desktop: '30.5%',
+                        monitor43: '30.5%',
+                    }
+                }}
+            >
+                <Box sx={{
+                    height: {
+                        lg: '335px',
+                        xl: '875px',
+                        desktop: '437px',
+                        monitor43: '875px',
+                    },
+                    width: '100vw',
+                }}
+                >
+                    <video
+                        muted
+                        loop
+                        style={{width: '100vw', height: 'inherit', objectFit: 'fill'}}
+                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                        autoPlay={true}
+                    />
+                </Box>
+            </Box>
+
         </Box>
     )
 };
