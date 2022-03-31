5ba120799f259711417edd736a7caf8b7c44a5e2
Thiti Watanasrimongkol <thitwata@scg.com>
2022-03-31 16:24:47 +0700

    Play sound in video

--- src/component/VideoPlayer/index.tsx
+++ src/component/VideoPlayer/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+// @ts-ignore
+import Silence from 'assets/sound/silence.mp3';
+// @ts-ignore
+import Septium from 'assets/sound/Septium.mp3';
 
 interface IVideo {
   onEnded: () => void;
@@ -8,15 +12,23 @@ interface IVideo {
 
 // ! policy google , autoplay must be muted
 const Video: React.FC<IVideo> = ({onEnded, source, loop = false}): React.ReactElement => {
+  // @ts-ignore
   return (
-    <video
-      muted
-      loop={loop}
-      onEnded={onEnded}
-      style={{height: '100%', width: '100%', objectFit: 'fill'}}
-      src={source}
-      autoPlay={true}
-    />
+    <>
+      <iframe src={Silence} allow='autoplay' id='audio' style={{ display:'none' }}/>
+      <audio autoPlay>
+        <source src={Septium} type='audio/mp3' />
+      </audio>
+      <video
+        muted={false}
+        loop={loop}
+        onEnded={onEnded}
+        style={{height: '100%', width: '100%', objectFit: 'fill'}}
+        src={source}
+        autoPlay={true}
+      />
+    </>
+
   );
 };
 
