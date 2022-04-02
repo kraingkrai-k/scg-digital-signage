a08ccae1798cd30676938bd49689176ac70affd6
krangkrai <kraingkrai.k96@gmail.com>
2022-04-02 17:03:39 +0700

    update config flashsale

--- src/pages/template3/model/flash-sale-data.tsx
+++ src/pages/template3/model/flash-sale-data.tsx
@@ -1,33 +1,32 @@
-
-import dayjs from 'dayjs';
+import dayjs, {Dayjs} from 'dayjs';
 
 // @ts-ignore
 import flashSaleVideo from 'assets/video/flash_sale.mp4';
 
 export interface IFlashSale {
   meta: string;
-  start: number;
-  end: number;
-  video: any
+  start: Dayjs;
+  end: Dayjs;
+  video: any;
 }
 
 export const flashSaleData: IFlashSale[] = [
   {
-    meta: '10.00 - 12.00',
-    start: dayjs().toDate().setHours(10),
-    end: dayjs().toDate().setHours(12),
-    video: flashSaleVideo
+    meta: '11.15 - 12.00',
+    start: dayjs().set('hour', 11).set('minute', 15),
+    end: dayjs().set('hour', 12).set('minute', 0),
+    video: flashSaleVideo,
   },
   {
-    meta: '12.00 - 14.00',
-    start: dayjs().toDate().setHours(12),
-    end: dayjs().toDate().setHours(14),
-    video: flashSaleVideo
+    meta: '14.15 - 15.00',
+    start: dayjs().set('hour', 14).set('minute', 15),
+    end: dayjs().set('hour', 15).set('minute', 0),
+    video: flashSaleVideo,
   },
   {
-    meta: '14.00 - 16.00',
-    start: dayjs().toDate().setHours(14),
-    end: dayjs().toDate().setHours(16),
-    video: flashSaleVideo
+    meta: '18.00 - 18.45',
+    start: dayjs().set('hour', 18).set('minute', 0),
+    end: dayjs().set('hour', 18).set('minute', 45),
+    video: flashSaleVideo,
   },
 ];
