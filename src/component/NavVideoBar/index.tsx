dc9571e918a774ebeab3be01969889182b259c02
krangkrai <kraingkrai.k96@gmail.com>
2022-04-02 15:43:52 +0700

    add fade video

--- src/component/NavVideoBar/index.tsx
+++ src/component/NavVideoBar/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
+import Fade from '@mui/material/Fade';
 
 import Navbar from 'component/Navbar';
 import VideoPlayer from '../VideoPlayer';
@@ -8,11 +9,26 @@ export interface INavVideoBar {
   video?: any;
 }
 
-const NavVideoBar: React.FC<INavVideoBar> = ({video}): React.ReactElement => {
+const MyComponent = React.forwardRef((props: any, ref: any) => {
+  return (
+    <div ref={ref} {...props}>
+      <VideoPlayer source={props?.source} loop={true} onEnded={() => {}} />
+    </div>
+  );
+});
 
-  if (!video) {
-    return <Navbar />
-  }
+const NavVideoBar: React.FC<INavVideoBar> = ({video}): React.ReactElement => {
+  const [fade, setFade] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (video) {
+      setTimeout(() => {
+        setFade(true);
+      }, 5000);
+    } else {
+      setFade(false);
+    }
+  }, [video]);
 
   return (
     <Box
@@ -21,7 +37,13 @@ const NavVideoBar: React.FC<INavVideoBar> = ({video}): React.ReactElement => {
         width: '100%',
       }}
     >
-      <VideoPlayer source={video} loop={true} onEnded={() => { }} />
+      {fade ? (
+        <Fade in={fade} unmountOnExit timeout={{enter: 3000}}>
+          <MyComponent source={video} loop={true} onEnded={() => {}} />
+        </Fade>
+      ) : (
+        <Navbar />
+      )}
     </Box>
   );
 };
