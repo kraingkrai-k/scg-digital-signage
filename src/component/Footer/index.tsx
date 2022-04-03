fa32b6907231906cbf89adb8e3429f7b4db0eafb
krangkrai <kraingkrai.k96@gmail.com>
2022-04-03 20:18:42 +0700

    update social qr

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -3,10 +3,14 @@ import Box from '@mui/material/Box';
 import SvgIcon from '@mui/material/SvgIcon';
 
 import {ReactComponent as FooterShape} from 'assets/icon/footerShape.svg';
-import {ReactComponent as Social} from 'assets/icon/social.svg';
 import Logo from 'assets/icon/logo.png';
-import {Title30} from 'component/common/Font.styles';
+import QRIG from 'assets/qr-social/IG.png';
+import QRFB from 'assets/qr-social/FB.png';
+import QRYT from 'assets/qr-social/Youtube.png';
+
+import {Title14, Title30} from 'component/common/Font.styles';
 import {COLORS} from 'core/utils/constant';
+import {IconTitleIG, IconTitleFB, IconTitleYT} from 'assets/icon/Icon';
 
 const Footer: React.FC = (): React.ReactElement => {
   return (
@@ -56,21 +60,15 @@ const Footer: React.FC = (): React.ReactElement => {
       <Box
         sx={{
           position: 'absolute',
-          display: 'grid',
-          bottom: '6%',
-          justifyItems: 'end',
-          width: '100%',
-          height: '10%',
+          display: 'flex',
+          bottom: '9%',
+          right: '3%',
+          gap: '12px',
         }}
       >
-        <SvgIcon
-          component={Social}
-          inheritViewBox
-          sx={{
-            width: '35%',
-            height: '90%',
-          }}
-        />
+        <IconSocial title="Facebook" qr={QRFB} icon={<IconTitleFB sx={{width: '16px', height: '16px'}} />} />
+        <IconSocial title="Instagram" qr={QRIG} icon={<IconTitleIG sx={{width: '16px', height: '16px'}} />} />
+        <IconSocial title="Youtube" qr={QRYT} icon={<IconTitleYT sx={{width: '16px', height: '16px'}} />} />
       </Box>
 
       <Box
@@ -91,5 +89,39 @@ const Footer: React.FC = (): React.ReactElement => {
     </Box>
   );
 };
+
+interface IIconSocial {
+  icon: any;
+  title: string;
+  qr: any;
+}
+
+const IconSocial = (props: IIconSocial) => (
+  <Box
+    sx={{
+      display: 'flex',
+      borderRadius: '8px',
+      flexDirection: 'column',
+      bgcolor: COLORS.white,
+      alignItems: 'center',
+      overflowY: 'hidden',
+    }}
+  >
+    <Box sx={{display: 'flex', alignItems: 'center', mt: '12px', position: 'relative'}}>
+      {props.icon}
+      <Title14 sx={{ml: '2px', fontWeight: 700}}>{props.title}</Title14>
+    </Box>
+    <Box
+      sx={{
+        backgroundImage: `url(${props.qr})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        width: '100px',
+        height: '100px',
+        mt: '-3%',
+      }}
+    />
+  </Box>
+);
 
 export default Footer;
