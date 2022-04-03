bdef30181fc6d620e4bcc2862d25b4c9ad316d2d
krangkrai <kraingkrai.k96@gmail.com>
2022-04-03 20:40:48 +0700

    add custom indicator carousel promotion

--- src/component/Promotion/index.tsx
+++ src/component/Promotion/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Carousel from 'react-material-ui-carousel';
 
 import Button from 'component/Button';
 import {ISectionPromotion, sectionPromotion} from 'pages/template3/model/promotion-data';
-import Carousel from 'react-material-ui-carousel';
+import {COLORS} from 'core/utils/constant';
 
 interface IPromotion {
   onPromotionClick: (x: ISectionPromotion) => void;
@@ -20,7 +21,33 @@ const Promotion: React.FC<IPromotion> = ({onPromotionClick, onProductClick}): Re
           cursor: 'pointer',
         }}
       >
-        <Carousel height="16vh" animation="slide" indicators={false} autoPlay={true} interval={5000}>
+        <Carousel
+          height="16vh"
+          animation="slide"
+          autoPlay={true}
+          interval={5000}
+          indicatorIconButtonProps={{
+            style: {
+              padding: '2px',
+              color: '#C0C0C0',
+            },
+          }}
+          activeIndicatorIconButtonProps={{
+            style: {
+              color: COLORS.red,
+            },
+          }}
+          indicatorContainerProps={{
+            style: {
+              position: 'absolute',
+              borderRadius: '60px',
+              paddingLeft: '12px',
+              paddingRight: '12px',
+              zIndex: 1,
+              bottom: '5%',
+            },
+          }}
+        >
           {sectionPromotion.map?.((x: ISectionPromotion, i: number) => (
             <Box
               key={i}
