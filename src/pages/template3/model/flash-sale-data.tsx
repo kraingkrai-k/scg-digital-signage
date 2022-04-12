9a3b2253e937c4a8b30cd04c5cb7bd482d699709
Thiti Watanasrimongkol <thitwata@scg.com>
2022-04-12 11:39:43 +0700

    remove flashsale after the end of xp innnovation

--- src/pages/template3/model/flash-sale-data.tsx
+++ src/pages/template3/model/flash-sale-data.tsx
@@ -11,22 +11,28 @@ export interface IFlashSale {
 }
 
 export const flashSaleData: IFlashSale[] = [
+  // {
+  //   meta: '11.15 - 12.00',
+  //   start: dayjs().set('hour', 11).set('minute', 15),
+  //   end: dayjs().set('hour', 12).set('minute', 0),
+  //   video: flashSaleVideo,
+  // },
+  // {
+  //   meta: '14.15 - 15.00',
+  //   start: dayjs().set('hour', 14).set('minute', 15),
+  //   end: dayjs().set('hour', 15).set('minute', 0),
+  //   video: flashSaleVideo,
+  // },
+  // {
+  //   meta: '18.00 - 18.45',
+  //   start: dayjs().set('hour', 18).set('minute', 0),
+  //   end: dayjs().set('hour', 18).set('minute', 45),
+  //   video: flashSaleVideo,
+  // },
   {
-    meta: '11.15 - 12.00',
-    start: dayjs().set('hour', 11).set('minute', 15),
-    end: dayjs().set('hour', 12).set('minute', 0),
-    video: flashSaleVideo,
-  },
-  {
-    meta: '14.15 - 15.00',
-    start: dayjs().set('hour', 14).set('minute', 15),
-    end: dayjs().set('hour', 15).set('minute', 0),
-    video: flashSaleVideo,
-  },
-  {
-    meta: '18.00 - 18.45',
-    start: dayjs().set('hour', 18).set('minute', 0),
-    end: dayjs().set('hour', 18).set('minute', 45),
-    video: flashSaleVideo,
-  },
+    meta: '00.00 - 00.00',
+    start: dayjs().set('hour', 0).set('minute',0),
+    end: dayjs().set('hour', 0).set('minute',0),
+    video: flashSaleVideo
+  }
 ];
